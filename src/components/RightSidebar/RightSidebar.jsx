4d0f71b89cee70ed7e5823292b5b56f8221eb8b9
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-25 14:40:32 +0500

    add RightSidebar Section

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,8 +1,28 @@
+import assets from "../../assets/assets"
 import "./RightSidebar.css"
 
 export const RightSidebar = () => {
   return (
-    <div>RightSidebar</div>
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>Richard Sanfors <img src={assets.green_dot} className="dot" alt="" /></h3>
+        <p>Hay, there i am Richard Sanford using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+        </div>
+      </div>
+      <button>Logout</button>
+    </div>
   )
 }
 export default RightSidebar
