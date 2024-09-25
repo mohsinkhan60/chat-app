1b85516bf0062770ef1a8f34b23c9ca05363522f
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2024-09-25 15:40:09 +0500

    add profile update Section

--- src/pages/profileUpdate/ProfileUpdate.jsx
+++ src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,8 +1,24 @@
 import "./ProfileUpdate.css"
+import assets from "../../assets/assets"
 
 export const ProfileUpdate = () => {
   return (
-    <div>ProfileUpdate</div>
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avater">
+            <input type="file" id="avater" accept=".png, .jpg, .jpeg" hidden />
+            <img src={assets.avatar_icon} alt="" />
+            upload profile image
+          </label>
+           <input required type="text" placeholder="your name..." />
+           <textarea placeholder="write profile bio" required></textarea>
+           <button type="submit">Save</button>
+        </form>
+        <img className="p-pic" src={assets.logo_icon} alt="" />
+      </div>
+    </div>
   )
 }
 export default ProfileUpdate
